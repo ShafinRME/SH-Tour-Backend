927e24fb94cdb0641c97716e17338b36c1e68c94
ShafinRME <shafin.rmedu@gmail.com>
2026-04-10 18:16:40 +0600

    super admin seed file created and user updation api ready

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,4 +1,5 @@
 import { Router } from "express"
+import { AuthRoutes } from "../modules/auth/auth.route"
 import { UserRoutes } from "../modules/user/user.route"
 
 export const router = Router()
@@ -8,10 +9,11 @@ const moduleRoutes = [
         path: "/user",
         route: UserRoutes
     },
-    // {
-    //     path: "/tour",
-    //     route: TourRoutes
-    // },
+    {
+        path: "/auth",
+        route: AuthRoutes
+    }
+
 ]
 
 moduleRoutes.forEach((route) => {
