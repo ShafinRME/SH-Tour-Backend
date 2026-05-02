d28dbe20e9d0d34617a5872ff2c7d15d1f6dbc0e
ShafinRME <shafin.rmedu@gmail.com>
2026-05-02 14:12:01 +0600

    sendemail part is updated

--- src/app/utils/sendEmail.ts
+++ src/app/utils/sendEmail.ts
@@ -7,14 +7,16 @@ import { envVars } from "../config/env";
 import AppError from "../errorHelpers/AppError";
 
 const transporter = nodemailer.createTransport({
-    // port: envVars.EMAIL_SENDER.SMTP_PORT,
-    secure: true,
+    host: envVars.EMAIL_SENDER.SMTP_HOST,
+    port: Number(envVars.EMAIL_SENDER.SMTP_PORT),
+    secure: Number(envVars.EMAIL_SENDER.SMTP_PORT) === 465,
     auth: {
         user: envVars.EMAIL_SENDER.SMTP_USER,
         pass: envVars.EMAIL_SENDER.SMTP_PASS
     },
-    port: Number(envVars.EMAIL_SENDER.SMTP_PORT),
-    host: envVars.EMAIL_SENDER.SMTP_HOST
+    tls: {
+        rejectUnauthorized: false
+    }
 })
 
 interface SendEmailOptions {
@@ -52,8 +54,8 @@ export const sendEmail = async ({
         })
         console.log(`\u2709\uFE0F Email sent to ${to}: ${info.messageId}`);
     } catch (error: any) {
-        console.log("email sending error", error.message);
-        throw new AppError(401, "Email error")
+        console.log("email sending error", error);
+        throw new AppError(500, "Email sending failed")
     }
 
 }
